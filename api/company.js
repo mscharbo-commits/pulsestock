d4ac8b2f2f453cace947659d320c6d009341130e
mscharbo-commits <mscharbo@gmail.com>
2026-07-06 20:57:22 -0400

    Update Finnhub key in company.js

--- api/company.js
+++ api/company.js
@@ -1,6 +1,6 @@
 export const config = { runtime: 'edge' };
 
-const FINNHUB_KEY = 'd8fhh6hr01qn443a0bngd8fhh6hr01qn443a0bo0';
+const FINNHUB_KEY = 'd95c889r01qihq3l33k0d95c889r01qihq3l33kg';
 
 async function getFinnhubProfile(ticker) {
   try {
